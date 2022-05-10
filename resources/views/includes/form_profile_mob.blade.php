93c4e47abf8759f922950565c1d41a31c3107e61
Agaly Omarov <97389793+agalyomarov@users.noreply.github.com>
2022-05-10 15:27:03 +0300

    srazu

--- resources/views/includes/form_profile_mob.blade.php
+++ resources/views/includes/form_profile_mob.blade.php
@@ -1,5 +1,5 @@
 @auth
-    <div class="form_profile mob">
+    <div class="form_profile mob" data-diller_balance="{{ auth()->user()->balance }}">
         <p class="name_profile">{{ auth()->user()->login }}</p>
         <p class="balance_profile">Ваш баланс<span class="sum">{{ auth()->user()->balance }}</span></p>
     </div>
--- resources/views/pay.blade.php
+++ resources/views/pay.blade.php
@@ -57,13 +57,13 @@
                             <p class="name_name">Название пакета</p>
                         </div>
                         <div class="item price_pay" style="text-align: center;">
-                            <p class="name_name" style="float: left">Цена в месяц</p>
+                            <p class="name_name" style="float: left">Цена в месяц (30 день)</p>
                             <p class="name_name" style="float: right;margin-right:20%">Информация</p>
                         </div>
                     </div>
                     <div class="content_pay_table">
                         <div class="item name_pay">
-                            <input type="checkbox" class="check_it" />
+                            <input type="checkbox" class="check_it inputVipPaket" data-price="1">
                             <p class="name_info">VIP</p>
                         </div>
                         <div class="item price_pay" style="text-align: center;margin-bottom:20px">
@@ -74,22 +74,23 @@
                 </div>
                 <div class="pay_sel ">
                     <p>Купить выбранные с</p>
-                    <input type="date">
+                    <input type="date" class="date_start_paket">
                     <p>по</p>
-                    <input type="date">
+                    <input type="date" class="date_end_paket">
                     <p>или купить на:</p>
                     <select>
-                        <option>1 месяц</option>
-                        <option>2 месяця</option>
-                        <option>3 месяця</option>
-                        <option>4 месяця</option>
-                        <option>5 месяцев</option>
-                        <option>6 месяцев</option>
-                        <option>1 год</option>
+                        <option value="1">1 месяц</option>
+                        <option value="2">2 месяця</option>
+                        <option value="3">3 месяця</option>
+                        <option value="4">4 месяця</option>
+                        <option value="5">5 месяцев</option>
+                        <option value="6">6 месяцев</option>
+                        <option value="12">1 год</option>
                     </select>
                 </div>
-                <div class="warning ">У вас пока нет выбранных пакетов...</div>
-                <a href="# " class="btn_pay ">Купить выбранный пакет</a>
+                <div class="warning message_for_diller">У вас пока нет выбранных пакетов...</div>
+                {{-- <a href="# " class="btn_pay ">Купить выбранный пакет</a> --}}
+                <input type="button" class="btn_pay " value="Купить выбранный пакет" style="font-size:15px;font-weight:bold">
             </div>
         </div>
     </section>
@@ -99,6 +100,35 @@
     </footer>
     <script src="https://code.jquery.com/jquery-2.2.0.min.js " type="text/javascript "></script>
     <!----- УДАЛИТЬ СЛАЙДЕР ----->
+    <script>
+        const messageForDiller = document.querySelector('.message_for_diller');
+        const inputVipPaket = document.querySelector('.inputVipPaket');
+        const dillerBalance = document.querySelector('.form_profile').dataset.diller_balance;
+        const packetPrice = inputVipPaket.dataset.price;
+        inputVipPaket.addEventListener('change', function(e) {
+            const dateStartPaket = document.querySelector('.date_start_paket');
+            const dateEndPaket = document.querySelector('.date_end_paket');
+            if (e.target.checked) {
+                if (!dateStartPaket.value || !dateEndPaket.value || dateStartPaket.value >= dateEndPaket.value) {
+                    messageForDiller.innerHTML = 'Выберите правилную дату ';
+                } else {
+                    const startDate = parseInt(parseInt(Date.parse(dateStartPaket.value)) / 1000);
+                    const endDate = parseInt(parseInt(Date.parse(dateEndPaket.value)) / 1000);
+                    const pricePaketForOneDay = (parseInt(e.target.dataset.price) / 30).toFixed(2);
+                    const differentDay = endDate - startDate;
+                    const generalPrice = (parseInt(differentDay / 86400) * pricePaketForOneDay).toFixed(2);
+                    console.log(differentDay);
+                    console.log(pricePaketForOneDay);
+                    console.log(generalPrice);
+                    messageForDiller.innerHTML = `Общая стоимость: ${generalPrice} $ Ваш баланс:  ${dillerBalance} $ | <a href='/pay_balance '>Пополнить</a> `;
+                }
+            } else {
+                messageForDiller.innerHTML = 'У вас пока нет выбранных пакетов...';
+            }
+        })
+    </script>
 </body>
 
 </html>
+{{-- Выберите правилную дату --}}
+{{-- Общая стоимость: ${currentPrice} $ Ваш баланс: ${balanceProfile} $ | <a href='/pay_balance'>Пополнить</a> --}}
