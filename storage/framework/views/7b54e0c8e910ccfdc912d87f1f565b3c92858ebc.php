198d052b1f96022574f878ed1c211c5119c6df84
Agaly Omarov <97389793+agalyomarov@users.noreply.github.com>
2022-05-20 13:28:27 +0300

    yesy

---
+++ storage/framework/views/7b54e0c8e910ccfdc912d87f1f565b3c92858ebc.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="format-detection" content="telephone=no" />
+    <link rel="stylesheet" href="<?php echo e(asset('css/framework.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('style.css?3')); ?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <script src="<?php echo e(asset('js/jquery.js')); ?>"></script>
+    <script src="<?php echo e(asset('js/js.js?4')); ?>"></script>
+    <title></title>
+</head>
+
+<body>
+    <?php echo $__env->make('includes.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <section class="home">
+        <div class="container clearfix">
+            <?php echo $__env->make('includes.form_profile_mob', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            <h1 class="our_clients">Новости</h1>
+            <div class="main_news">
+                <?php if(isset($posts) && count($posts) > 0): ?>
+                    <?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <div class="item">
+                            <p class="date"><?php echo $post->title; ?></p>
+                            <p class="text"><?php echo $post->content; ?></p>
+                        </div>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                <?php endif; ?>
+
+                
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="container clearfix">
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+    <!----- УДАЛИТЬ СЛАЙДЕР ----->
+</body>
+
+</html>
+<?php /**PATH /Users/agaly/Desktop/tmsat/resources/views/news.blade.php ENDPATH**/ ?>
